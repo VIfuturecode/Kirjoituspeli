8727ac1f7ca7288eebd8e1d3b1d8bf999dafd371
Viktoria Vetserkovskaja <vetserkovskaja@gmail.com>
2025-12-19 01:48:41 +0200

    Luotu game.js ja lisätty DOM-elementtien viitteet

---
+++ js/game.js
@@ -0,0 +1,17 @@
+/* DOM */
+const gameScene = document.getElementById("game-scene");
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+const inputText = document.getElementById("input-text");
+const pauseBtn = document.getElementById("pause-btn");
+const resumeBtn = document.getElementById("resume-btn");
+const restartBtn = document.getElementById("restart-btn");
+const menuBtn = document.getElementById("menu-btn");
+const pauseOverlay = document.getElementById("pause-overlay");
+const gameoverOverlay = document.getElementById("gameover-overlay");
+const scoreDisplay = document.getElementById("score");
+const finalScoreDisplay = document.getElementById("final-score");
+const livesDisplay = document.getElementById("lives");
+const difficultyBadge = document.getElementById("difficulty-badge");
+const difficultyCards = document.querySelectorAll(".difficulty-card");
+const muteBtnGame = document.getElementById("mute-btn-game");
