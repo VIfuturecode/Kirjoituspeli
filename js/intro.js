34b4d8c0a8ddd9e3dc67053ac402be24cc7a6da8
Viktoria Vetserkovskaja <vetserkovskaja@gmail.com>
2025-12-19 01:29:40 +0200

    Lisätty intron DOM-elementtien valinnat

--- js/intro.js
+++ js/intro.js
@@ -7,5 +7,14 @@ const characters = [
   
   let currentCharacterIndex = 0;
   let isMuted = false;
-  
+/* DOM */
+const introScene = document.getElementById("intro-scene");
+const menuScene = document.getElementById("menu-scene");
+const characterAvatar = document.getElementById("character-avatar");
+const characterName = document.getElementById("character-name");
+const dialogueText = document.getElementById("dialogue-text");
+const progressDots = document.getElementById("progress-dots");
+const skipBtn = document.getElementById("skip-btn");
+const muteBtn = document.getElementById("mute-btn");
+const replayBtn = document.getElementById("replay-btn");
   
