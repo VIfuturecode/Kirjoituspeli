612cc507fe8468a7f3dc5fe89f081ffc43007d11
Viktoria Vetserkovskaja <vetserkovskaja@gmail.com>
2025-12-19 01:25:07 +0200

    Luotu intro.js ja lisätty hahmojen määrittelyt

---
+++ js/intro.js
@@ -0,0 +1,11 @@
+/* HAHMOT  */
+const characters = [
+    { id: "wizard", name: "Velho Viisas", dialogue: "Tervetuloa seikkailuun!", avatar: "🧙", cssClass: "wizard" },
+    { id: "knight", name: "Ritari Rohkea", dialogue: "Maailma on vaarassa...", avatar: "⚔️", cssClass: "knight" },
+    { id: "sage", name: "Tietäjä", dialogue: "Valitse matkasi viisaasti.", avatar: "🔮", cssClass: "sage" }
+  ];
+  
+  let currentCharacterIndex = 0;
+  let isMuted = false;
+  
+  
